74744f2a60158d09195ae101735c0200b8c93f70
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-07-14 20:35:38 -0400

    Time: 157 ms (60.79%) | Memory: 30.8 MB (5.03%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,34 +1,35 @@
 class Solution:
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        check = {i:0 for i in wordList}
-        if endWord not in check:
-            return 0
-        def oneChange(new, old) -> bool:
-            c = 0
-            for i in range(len(new)):
-                if new[i] != old[i]:
-                    c += 1
-
-            return c == 1
-
-        q = collections.deque()
-        q.append(beginWord)
+    def ladderLength(self, begin_word: str, end_word: str, word_list: List[str]) -> int:
+        if begin_word not in word_list:
+            word_list.append(begin_word)
+        patterns = defaultdict(set)
+        for word in word_list:
+            for i in range(len(word)):
+                form = word[:i] + "*" + word[i+1:]
+                patterns[form].add(word)
+    
+        # Create a graph between words
+        graph = defaultdict(list)
+        for word in word_list:
+            local_set = set()
+            for i in range(len(word)):
+                form = word[:i] + "*" + word[i+1:]
+                local_set |= patterns[form]
+            graph[word] = list(local_set)
+        # find the shortest way between begin_word and end_word
+        # -> BFS
+        queue = deque([begin_word])
         step = 0
-        
-        while q:
-            length = len(q)
-            # print(q)
-            for i in range(length):
-                checkWord = q.popleft()
-                if checkWord == endWord:
-                    return step + 1
-                if step != 0 and check[checkWord] != 0:
-                    wordList.remove(checkWord)
-                for word in wordList:
-                    if oneChange(checkWord, word) and check[word] == 0:
-                        check[word] = 1
-                        q.append(word)
-            
+        visit = set([begin_word])
+        while queue:
             step += 1
-
+            for _ in range(len(queue)):
+                choosen_word = queue.popleft()
+                if choosen_word == end_word:
+                    return step
+                for next_word in graph[choosen_word]:
+                    if next_word in visit:
+                        continue
+                    visit.add(next_word)
+                    queue.append(next_word)
         return 0
