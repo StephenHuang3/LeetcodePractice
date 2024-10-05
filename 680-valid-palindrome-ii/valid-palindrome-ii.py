a7292f01bb158260f2a4feb0d49d96c5aec95134
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-05 13:32:03 -0400

    Time: 113 ms (23.40%) | Memory: 17.1 MB (12.30%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,14 +1,31 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def valid(s, fudge):
-            if len(s) <= 1:
-                return True
-            elif s[0] == s[-1]:
-                return valid(s[1:-1], fudge)
-            elif fudge == 1:
-                return False
-            else:
-                return valid(s[:-1], 1) or valid(s[1:], 1)
+        breakpoint = -1
+        for i in range(len(s) // 2):
+            if s[i] != s[-i - 1]:
+                breakpoint = i
+                break
+        
+        if breakpoint == -1:
+            return True
 
+        print("breakpoint")
+        print(breakpoint)
 
-        return valid(s, 0)
+        found_left = True
+        found_right = True
+
+        for i in range(breakpoint, len(s) // 2):
+            if s[i + 1] != s[-i - 1]:
+                found_left = False
+                break
+
+        for i in range(breakpoint, len(s) // 2):
+            if s[i] != s[-i - 2]:
+                found_right = False
+                print("i")
+                print(i)
+                print(s[-i - 2]) 
+                break
+        
+        return found_left or found_right
