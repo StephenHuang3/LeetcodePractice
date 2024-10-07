fcee58c768b6f682fe95a0047767c728378d53bd
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-07 18:43:14 -0400

    Time: 242 ms (17.03%) | Memory: 19.3 MB (97.54%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = {}
+        count[0] = 1
+        cur_sum = 0
+        res = 0
+
+        for n in nums:
+            cur_sum += n
+            need = cur_sum - k
+            if need in count:
+                res += count[need]
+            if cur_sum not in count:
+                count[cur_sum] = 0
+            count[cur_sum] += 1
+        print(count)
+        return res
