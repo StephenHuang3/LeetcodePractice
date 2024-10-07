11fe5b932751149d9de8d7eff9c443a836f0c5a1
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-07 00:19:52 -0400

    Time: 55 ms (14.21%) | Memory: 21.8 MB (7.87%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,11 +1,5 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = ''.join(char for char in s if char.isalnum())
-        s = s.upper()
-        print(s)
-        n = len(s) - 1
-        for i in range(len(s) // 2):
-            if s[i] != s[n - i]:
-                return False
-        return True
+        s = [c.lower() for c in s if c.isalnum()]
+        return all (s[i] == s[-i - 1] for i in range(len(s)//2))
