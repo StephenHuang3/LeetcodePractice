0922c2db01aeca919462cde79ddd8b5f1daddd7f
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-07 01:46:50 -0400

    Time: 28 ms (91.16%) | Memory: 16.6 MB (20.35%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,4 +1,25 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+        if x == 0:
+            return 0
+        if n == 1:
+            return x
+        
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1/x
+            n = -n
+
+        odd_power = 1
+        even_power = x
+        while n > 1:
+            if n % 2 == 1:
+                odd_power *= even_power
+                n = n - 1
+
+            even_power *= even_power
+            n = n // 2
+
+        return even_power * odd_power
         
