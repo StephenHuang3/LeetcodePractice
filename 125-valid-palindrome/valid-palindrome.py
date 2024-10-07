7750aedbb78e960880301462161c672f78a89e6c
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-07 00:20:12 -0400

    Time: 45 ms (56.33%) | Memory: 21.9 MB (7.87%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,4 +2,4 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = [c.lower() for c in s if c.isalnum()]
-        return all (s[i] == s[-i - 1] for i in range(len(s)//2))
+        return all (s[i] == s[~i] for i in range(len(s)//2))
