57e396eae0765091c8810f331c1953f6825f4cd5
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-07 00:01:33 -0400

    Time: 111 ms (49.45%) | Memory: 19.9 MB (44.13%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,67 +1,31 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        def cal(op1, op2, operand):
-            if operand == "+":
-                return op1 + op2
-            if operand == "-":
-                return op1 - op2
-            if operand == "*":
-                return op1 * op2
-            if operand == "/":
-                return op1 // op2
+        stack = []
+        num = ""
+        prev_operator = '+'
 
-        stk = []
-        i = 0
-        while i < len(s):
-            if s[i] == " ":
-                i += 1
-                continue
-            elif s[i] == "/" or s[i] == "*":
-                stk.append(s[i])
-                i += 1
+        for i in range(len(s) + 1):
+            ch = s[i] if i < len(s) else '\0'
 
-                while s[i] == " ":
-                    i += 1
+            if ch.isdigit():
+                num += ch
+            elif ch != " " or i == len(s):
+                match prev_operator:
+                    case "+":
+                        stack.append(int(num))
+                    case "-":
+                        stack.append(-1 * int(num))
+                    case "*":
+                        op1 = stack.pop()
+                        stack.append(op1 * int(num))
+                    case "/":
+                        op1 = stack.pop()
+                        stack.append(int(op1 / int(num)))
 
-                c = 1
-                while i + c < len(s) and s[i + c].isnumeric():
-                    c += 1
+                    
+                prev_operator = ch
+                num = ""
 
-                op2 = int(s[i: i + c])
-                operand = stk.pop()
-                op1 = stk.pop()
-                stk.append(cal(op1, op2, operand))
-                i = c + i - 1
-            elif s[i].isnumeric():
-                c = 1
-                while i + c < len(s) and s[i + c].isnumeric():
-                    c += 1
-                stk.append(int(s[i: i + c]))
-                i = c + i - 1
-            else:
-                stk.append(s[i])
-            i += 1
-
-        print(stk)
-        if len(stk) == 1:
-            return stk[0]
-
-        i = 0
-        stk2 = []
-        while i < len(stk):
-            if isinstance(stk[i], int):
-                stk2.append(stk[i])
-            else:
-                operand = stk[i]
-                i += 1
-                op2 = stk[i]
-                op1 = stk2.pop()
-                stk2.append(cal(op1, op2, operand))
-
-            i += 1
-
-        return stk2[0]
-
-
+        return sum(stack)
 
         
