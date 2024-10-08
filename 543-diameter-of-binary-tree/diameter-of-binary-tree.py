3da7ce8a9b797eff03bda5fa3e3f020f5544b19a
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-07 22:06:34 -0400

    Time: 46 ms (67.76%) | Memory: 19.8 MB (5.65%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -12,17 +12,17 @@ class Solution:
             return 0
         
         def recur(node, depth):
-            print("node depth", node.val, depth)
+            # print("node depth", node.val, depth)
             if not node.left and not node.right:
                 return depth
 
             nonlocal max_diameter
             left = recur(node.left, depth + 1) if node.left else 0
             right = recur(node.right, depth + 1) if node.right else 0
-            print("left right", left, right)
+            # print("left right", left, right)
             max_diameter = max(max_diameter, left - depth + right - depth)
 
             return max(left, right)
         
-        print(recur(root, 0))
+        recur(root, 0)
         return max_diameter
