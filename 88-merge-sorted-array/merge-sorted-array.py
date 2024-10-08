d2ff861fc5aa746540261d5d561884873ff00f43
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-07 23:11:50 -0400

    Time: 40 ms (52.13%) | Memory: 16.7 MB (15.42%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,26 +1,34 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        c1 = 0
-        c2 = 0
-        output = []
-        
-        while c1 < m and c2 < n:
-            if nums1[c1] < nums2[c2]:
-                output.append(nums1[c1])
-                c1 += 1
-            else:
-                output.append(nums2[c2])
-                c2 += 1
-            
-        print(output)
-        if c1 < m:
-            output = output + nums1[c1:m]
-        if c2 < n:
-            output = output + nums2[c2:]
+        # right = m + n - 1
+        # idx1 = m - 1
+        # idx2 = n - 1
 
-        for i in range(len(output)):
-            nums1[i] = output[i]
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
+        # if len(nums2) == 0:
+        #     return
+        
+
+        # while idx1 >= 0:
+        #     if idx2 < 0 or (idx1 >= 0 and nums1[idx1] > nums2[idx2]):
+        #         nums1[right] = nums1[idx1]
+        #         idx1 -= 1
+        #     else:
+        #         print(idx2)
+        #         nums1[right] = nums2[idx2]
+        #         idx2 -= 1
+
+        #     right -= 1
+        midx = m - 1
+        nidx = n - 1 
+        right = m + n - 1
+
+        while nidx >= 0:
+            if midx >= 0 and nums1[midx] > nums2[nidx]:
+                nums1[right] = nums1[midx]
+                midx -= 1
+            else:
+                nums1[right] = nums2[nidx]
+                nidx -= 1
+
+            right -= 1
         
