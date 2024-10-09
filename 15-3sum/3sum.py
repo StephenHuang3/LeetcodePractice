98c182a209f5ae6e245997d2eaf0ffb357194315
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-10-09 00:11:47 -0400

    Time: 1367 ms (15.95%) | Memory: 22.3 MB (5.89%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        found = set()
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            target = -nums[i]
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                cur_sum = nums[l] + nums[r]
+                if cur_sum == target:
+                    if (nums[i], nums[l], nums[r]) not in found:
+                        res.append([nums[i], nums[l], nums[r]])
+                        found.add((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+                    # print("num l r", nums[l], nums[r])
+                elif cur_sum < target:
+                    l += 1
+                else:
+                    r -= 1
+
+        return res
