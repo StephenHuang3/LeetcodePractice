57a7a1983385a6af657db34aa12262bf11e0386d
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2024-12-15 16:37:17 -0500

    Time: 12 ms (42.80%) | Memory: 17.4 MB (20.28%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        n = len(candidates)
 
         def backtrack(pos, comb, total):
-            if pos >= (len(candidates)) or target < total:
+            nonlocal n
+            if pos >= n or target < total:
                 return
 
             if target == total:
