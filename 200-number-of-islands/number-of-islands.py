f5a103f6329e446de51ee2a484bf22584b7806b9
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-02-02 15:14:27 -0500

    Time: 294 ms (17.77%) | Memory: 28.3 MB (9.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         visited = set()
+        dir = [(0,1), (1, 0), (0, -1), (-1, 0)]
         count = 0
-        row, col = len(grid), len(grid[0])
+        R = len(grid)
+        C = len(grid[0])
 
         def dfs(r, c):
-            if r < 0 or c < 0 or r >= row or c >= col or (r,c) in visited:
-                return
-            
-            visited.add((r,c))
-            if grid[r][c] == "1":
-                dfs(r + 1, c)
-                dfs(r, c + 1)
-                dfs(r - 1, c)
-                dfs(r, c - 1)
-            return
+            print("R c", r, c)
+            visited.add((r, c))
+            for dy, dx in dir:
+                if 0 <= r + dx < R and 0 <= c + dy < C and grid[r + dx][c + dy] == "1" and (r + dx, c + dy) not in visited:
+                    dfs(r + dx, c + dy)
 
-        
-        
-
-        for r in range(row):
-            for c in range(col):
-                if (r, c) not in visited and grid[r][c] == "1":
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == "1" and (r, c) not in visited:
                     count += 1
+                    print("trigger", r, c)
                     dfs(r, c)
-
+                    
         return count
