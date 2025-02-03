2027fd0b7128589508e47a17632afc7b90d7e8ee
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-02-02 22:59:30 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (65.55%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -8,13 +8,13 @@ class Solution:
         open.add("{")
 
         for bra in s:
-            print(bra)
-            print(bra in open)
+            # print(bra)
+            # print(bra in open)
             if bra in open:
                 stk.append(bra)
             else:
                 if len(stk) == 0:
-                    print("len 0", bra)
+                    # print("len 0", bra)
                     return False
                 popped = stk.pop()
                 if bra == ")" and popped != "(":
