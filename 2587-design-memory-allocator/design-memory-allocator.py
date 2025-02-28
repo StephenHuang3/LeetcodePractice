a20ca9c9288ad68a6ce60d701bf4995a90cb4b2a
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-02-28 01:20:54 -0500

    Time: 16 ms (89.53%) | Memory: 18.5 MB (69.85%) - LeetSync

--- 2587-design-memory-allocator/design-memory-allocator.py
+++ 2587-design-memory-allocator/design-memory-allocator.py
@@ -1,32 +1,47 @@
+from collections import defaultdict
+from bisect import insort
+
+
 class Allocator:
 
     def __init__(self, n: int):
-        self.mem = [0] * n
+        self.free = [(0, n - 1)]
+        self.allocations = defaultdict(list)
         self.n = n
 
     def allocate(self, size: int, mID: int) -> int:
-        cnt = 0
-        for i in range(self.n):
-            if self.mem[i] == 0:
-                cnt += 1
-            else:
-                cnt = 0
-            
-            if cnt == size:
-                for j in range(i, i - size, -1):
-                    self.mem[j] = mID
-                return i - size + 1
-
+        if size > self.n:
+            return -1
+        for i, (s, e) in enumerate(self.free):
+            if e - s + 1 >= size:
+                self.allocations[mID].append((s, s + size - 1))
+                if e > s + size - 1:
+                    self.free[i] = (s + size, e)
+                else:
+                    self.free.pop(i)
+                return s
         return -1
-        
 
     def freeMemory(self, mID: int) -> int:
-        c = 0
-        for i in range(self.n):
-            if self.mem[i] == mID:
-                self.mem[i] = 0
-                c += 1
-        return c
+        if not self.allocations[mID]:
+            return 0
+        
+        freed = 0
+        for s, e in self.allocations.pop(mID):
+            insort(self.free, (s, e))
+            freed += e - s + 1
+
+        new = [self.free[0]]
+        for i, (s, e) in enumerate(self.free[1:]):
+            if s <= new[-1][1] + 1:
+                new[-1] = (new[-1][0], max(e, new[-1][1]))
+            else:
+                new.append((s, e))
+
+        self.free = new
+        return freed
+
+        
 
 
 # Your Allocator object will be instantiated and called as such:
