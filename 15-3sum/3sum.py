a8383b950601c2dd9d663b14d9479505c671bcd4
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-09-20 18:17:55 -0400

    Time: 848 ms (19.94%) | Memory: 20.2 MB (94.34%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,35 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        found = set()
         nums.sort()
+        found = set()
 
         for i in range(len(nums) - 2):
-            if nums[i] > 0:
-                break
-
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-                
-            target = -nums[i]
             l = i + 1
             r = len(nums) - 1
+
             while l < r:
-                cur_sum = nums[l] + nums[r]
-                if cur_sum == target:
-                    if (nums[i], nums[l], nums[r]) not in found:
-                        res.append([nums[i], nums[l], nums[r]])
-                        found.add((nums[i], nums[l], nums[r]))
+                if nums[l] + nums[r] == -nums[i]:
+                    found.add((nums[i], nums[l], nums[r]))
                     l += 1
                     r -= 1
-                    while nums[l]==nums[l-1] and l<r:
-                        l+=1
-                    while nums[r]==nums[r+1] and l<r:
-                        r-=1
-                    # print("num l r", nums[l], nums[r])
-                elif cur_sum < target:
+                elif nums[l] + nums[r] < -nums[i]:
                     l += 1
                 else:
                     r -= 1
 
-        return res
+        return list(found)
