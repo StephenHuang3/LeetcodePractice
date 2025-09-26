b43bc1193182d6c923ea21dc5a0f78985eff7fb4
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-09-26 02:17:42 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (42.02%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -3,31 +3,27 @@ class Solution:
         l = 0
         r = len(nums) - 1
 
-        while l < r:
+        while l <= r:
             m = (l + r) // 2
-            # print("cur m", nums[m])
             if nums[m] == target:
                 return m
-            # print(nums[l], nums[r], nums[m])
 
             if nums[l] <= nums[m] <= nums[r]:
                 if target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
-            elif nums[r] <= nums[l] <= nums[m]:
+
+            elif nums[m] < nums[l]:
+                if nums[m] <= target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+
+            else:
                 if nums[l] <= target <= nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
-            elif nums[m] <= nums[r] <= nums[l]:
-                if nums[m] <= target <= nums[r]:
-                    l = m + 1
-                else:
-                    r = m - 1            
-            
-        
-        if nums[l] == target:
-            return l
+
         return -1
-            
