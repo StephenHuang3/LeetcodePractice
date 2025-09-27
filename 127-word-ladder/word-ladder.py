c103edd1182b813795fa6b746efe0fb9133a7f3b
Stephen Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-09-27 00:54:49 -0400

    Time: 781 ms (15.60%) | Memory: 31.4 MB (5.12%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -4,16 +4,18 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         seen = set()
         seen.add(beginWord)
-        pattern_to_word = defaultdict(list)
+        pattern_to_word = defaultdict(set)
 
         for word in wordList:
             for i in range(len(beginWord)):
                 pattern = word[:i] + "*" + word[i + 1:]
-                pattern_to_word[pattern].append(word)
+                pattern_to_word[pattern].add(word)
 
         level = 1
         q = deque()
         q.append(beginWord)
+        seen.add(beginWord)
+
         while q:
             length = len(q)
             for i in range(length):
@@ -23,9 +25,10 @@ class Solution:
                     return level
                 for i in range(len(beginWord)):
                     pattern = word[:i] + "*" + word[i + 1:]
-                    for cand in pattern_to_word[pattern]:
-                        if cand not in seen:
-                            q.append(cand)
+                    q.extend(list(pattern_to_word[pattern].difference(seen)))
+                    # for cand in pattern_to_word[pattern]:
+                    #     if cand not in seen:
+                    #         q.append(cand)
             level += 1
 
         return 0
