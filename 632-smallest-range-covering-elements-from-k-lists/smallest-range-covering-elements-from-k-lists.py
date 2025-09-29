6d542967735e2496fb0649c0a44328c826d9ecfe
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-09-28 22:44:38 -0400

    Time: 271 ms (73.27%) | Memory: 38.5 MB (97.06%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -1,34 +1,27 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        heap = []
-        retmax = nums[0][0]
+        # have a heap(num, idx, which arr)
+        hp = []
+        max_val = float("-inf")
+        cur_max_val = float("-inf")
 
         for i in range(len(nums)):
-            nums[i].sort()
-            heapq.heappush(heap, (nums[i][0], i, 0))
-            retmax = max(retmax, nums[i][0])
-        
-        retmin = heap[0][0]
-        curmin = retmin
-        curmax = retmax
-        curminrange = retmax - retmin
+            heapq.heappush(hp, (nums[i][0], 0, i))
+            max_val = max(max_val, nums[i][0])
 
-        while len(heap) > 0:
-            num, row, idx = heapq.heappop(heap)
-            
-            if idx + 1 == len(nums[row]):
+        min_val = hp[0][0]
+        cur_max_val = max_val
+
+        while True:
+            val, idx, arr_idx = heapq.heappop(hp)
+            if len(nums[arr_idx]) == idx + 1:
                 break
-            heapq.heappush(heap, (nums[row][idx + 1], row, idx + 1))
-            if nums[row][idx + 1] > curmax:
-                curmax = nums[row][idx + 1]
-            curmin = heap[0][0]
+            heapq.heappush(hp, (nums[arr_idx][idx + 1], idx + 1, arr_idx))
+            cur_max_val = max(nums[arr_idx][idx + 1], cur_max_val)
+            cur_min_val = hp[0][0]
 
-            # print("max min")
-            # print(curmax)
-            # print(curmin)
-            if curmax - curmin < curminrange:
-                retmax = curmax
-                retmin = curmin
-                curminrange = curmax - curmin
-
-        return [retmin, retmax]
+            if max_val - min_val > cur_max_val - cur_min_val:
+                max_val = cur_max_val
+                min_val = cur_min_val
+        
+        return [min_val, max_val]
