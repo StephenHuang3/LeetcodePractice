971ca0422f6922561f81544887440e4895e7f252
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-10-03 02:38:01 -0400

    Time: 95 ms (14.66%) | Memory: 19 MB (77.56%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -0,0 +1,22 @@
+from collections import deque
+import math
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+
+        cost = [float("inf")] * n
+        cost[src] = 0
+
+        for i in range(k + 1):
+            prev = cost.copy()
+            for start, end, price in flights:
+                if prev[start] != float("inf") and cost[end] > prev[start] + price:
+                    cost[end] = prev[start] + price
+
+
+        if cost[dst] == float("inf"):
+            return -1
+        return cost[dst]
+
+       
