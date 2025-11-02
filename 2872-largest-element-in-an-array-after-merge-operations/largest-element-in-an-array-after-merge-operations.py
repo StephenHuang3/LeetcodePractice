ddbc56242fe366f6cd42ba9347765f413d895868
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-11-01 22:43:26 -0700

    Time: 36 ms (77.56%) | Memory: 33.6 MB (78.85%) - LeetSync

--- 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.py
+++ 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxArrayValue(self, nums: List[int]) -> int:
-        max_found = nums[-1]
-        while len(nums) > 1:
-            max_found = max(max_found, nums[-1])
-            if nums[-2] <= nums[-1]:
-                popped = nums.pop()
-                nums[-1] += popped
-            else:
-                nums.pop()
+        max_found = 0
 
-        return max(max_found, nums[0])
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] <= max_found:
+                max_found += nums[i]
+            else:
+                max_found = nums[i]
+
+        return max_found
