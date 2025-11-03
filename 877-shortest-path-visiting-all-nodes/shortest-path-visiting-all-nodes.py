749a3662b007a0e765723016a9d3d4a348e7f41e
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-11-03 01:02:59 -0800

    Time: 123 ms (35.09%) | Memory: 22.4 MB (49.07%) - LeetSync

---
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        n = len(graph)
+        q = deque([(1 << i, i, 0) for i in range(n)]) # (mask, pos, distance)
+        visited = set()
+
+        while q:
+            mask, cur_pos, dist = q.popleft()
+            if mask == (1 << n) - 1:
+                return dist
+            for nei in graph[cur_pos]:
+                new_mask = mask | 1 << nei
+                if (new_mask, nei) not in visited:
+                    q.append((new_mask, nei, dist + 1))
+                    visited.add((new_mask, nei))
