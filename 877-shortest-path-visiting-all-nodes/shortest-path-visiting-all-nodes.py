e78cb850ec5788bd59b5ff3e3f54fb6e1f712e8e
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-11-03 01:04:03 -0800

    Time: 111 ms (74.07%) | Memory: 22.5 MB (32.45%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.py
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.py
@@ -6,10 +6,11 @@ class Solution:
         n = len(graph)
         q = deque([(1 << i, i, 0) for i in range(n)]) # (mask, pos, distance)
         visited = set()
+        finish_mask = (1 << n) - 1
 
         while q:
             mask, cur_pos, dist = q.popleft()
-            if mask == (1 << n) - 1:
+            if mask == finish_mask:
                 return dist
             for nei in graph[cur_pos]:
                 new_mask = mask | 1 << nei
