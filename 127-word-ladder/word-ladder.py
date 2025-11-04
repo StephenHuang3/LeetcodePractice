7cf1fa2ea5f45670ebd5643c7516d3265594e7f9
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-11-03 18:13:54 -0800

    Time: 74 ms (65.91%) | Memory: 25.4 MB (12.15%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -2,33 +2,27 @@ from collections import defaultdict, deque
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        hp = defaultdict(list)
         seen = set()
-        seen.add(beginWord)
-        pattern_to_word = defaultdict(set)
-
         for word in wordList:
-            for i in range(len(beginWord)):
-                pattern = word[:i] + "*" + word[i + 1:]
-                pattern_to_word[pattern].add(word)
+            for i in range(len(word)):
+                key = word[:i] + '*' + word[i + 1:]
+                hp[key].append(word)
 
-        level = 1
-        q = deque()
-        q.append(beginWord)
+        q = collections.deque()
+        q.append((beginWord, 1))
         seen.add(beginWord)
-
+        
         while q:
-            length = len(q)
-            for i in range(length):
-                word = q.popleft()
-                seen.add(word)
-                if word == endWord:
-                    return level
-                for i in range(len(beginWord)):
-                    pattern = word[:i] + "*" + word[i + 1:]
-                    q.extend(list(pattern_to_word[pattern].difference(seen)))
-                    # for cand in pattern_to_word[pattern]:
-                    #     if cand not in seen:
-                    #         q.append(cand)
-            level += 1
+            word, dis = q.popleft()
+            if word == endWord:
+                return dis
+
+            for i in range(len(word)):
+                key = word[:i] + '*' + word[i + 1:]
+                for connection_word in hp[key]:
+                    if connection_word not in seen:
+                        seen.add(connection_word)
+                        q.append((connection_word, dis + 1))
 
         return 0
