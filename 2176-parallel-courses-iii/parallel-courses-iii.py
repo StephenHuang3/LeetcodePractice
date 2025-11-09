281dd89461a20a31679fe26b139071fddd0aee1b
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-11-09 12:54:17 -0800

    Time: 154 ms (48.32%) | Memory: 55.5 MB (20.51%) - LeetSync

--- 2176-parallel-courses-iii/parallel-courses-iii.py
+++ 2176-parallel-courses-iii/parallel-courses-iii.py
@@ -4,25 +4,29 @@ import heapq
 
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        leadsto = defaultdict(list)
-        prereqs = defaultdict(set)
-
+        prereqs = defaultdict(list)
+        max_time = {}
+        
         for prev, nxt in relations:
-            leadsto[prev].append(nxt)
-            prereqs[nxt].add(prev)
+            prereqs[nxt].append(prev)
 
-        hp = []
-        cur_time = 0
+        
+        def dfs(course):
+            if course in max_time:
+                return max_time[course]
+            
+            course_time = time[course - 1]
+            dependents_time = 0
+
+            for req in prereqs[course]:
+                dependents_time = max(dependents_time, dfs(req))
+
+            max_time[course] = course_time + dependents_time
+
+            return course_time + dependents_time
+
         for i in range(1, n + 1, 1):
-            if not prereqs[i]:
-                heapq.heappush(hp, (time[i - 1] + cur_time, i))
-        # print(hp)
-        while hp:
-            finish_time, finished_course = heapq.heappop(hp)
-            cur_time = finish_time
-            for nxt in leadsto[finished_course]:
-                prereqs[nxt].discard(finished_course)
-                if len(prereqs[nxt]) == 0:
-                    heapq.heappush(hp, (cur_time + time[nxt - 1], nxt))
+            dfs(i)
+        return max(max_time.values())
 
-        return cur_time
+            
