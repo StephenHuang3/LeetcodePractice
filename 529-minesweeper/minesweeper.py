bcfddb4faaaf90d98e8a952994ca788836173aca
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-11-19 20:42:10 -0800

    Time: 7 ms (60.62%) | Memory: 18.5 MB (71.18%) - LeetSync

--- 529-minesweeper/minesweeper.py
+++ 529-minesweeper/minesweeper.py
@@ -5,6 +5,10 @@ class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         R = len(board)
         C = len(board[0])
+        if board[click[0]][click[1]] == 'M':
+            board[click[0]][click[1]] = 'X'
+            return board
+
         dir = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
 
         def bomb_nei(r,c):
@@ -13,7 +17,7 @@ class Solution:
             for dr, dc in dir:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < R and 0 <= nc < C:
-                    if ret[nr][nc] == 'M':
+                    if board[nr][nc] == 'M':
                         count += 1
 
             return count
@@ -28,27 +32,25 @@ class Solution:
                 num_nei = bomb_nei(pr, pc)
                 if num_nei == 0:
                     # print(pr, pc, "zero bombs")
-                    ret[pr][pc] = 'B'
+                    board[pr][pc] = 'B'
                     for dr, dc in dir:
                         nr, nc = pr + dr, pc + dc
                         if 0 <= nr < R and 0 <= nc < C and (nr, nc) not in seen:
                             seen.add((nr, nc))
                             q.append((nr, nc))
                 else:
-                    ret[pr][pc] = str(num_nei)
-
-        ret = copy.deepcopy(board)
-
+                    board[pr][pc] = str(num_nei)
+                    
         r, c = click[0], click[1]
-        if ret[r][c] == "M":
-            ret[r][c] = 'X'
-            return ret
-        elif ret[r][c] == 'E':
+        if board[r][c] == "M":
+            board[r][c] = 'X'
+            return board
+        elif board[r][c] == 'E':
             num_nei = bomb_nei(r, c)
             if num_nei == 0:
                 reveal_recursive(r, c)
             else:
-                ret[r][c] = str(num_nei)
+                board[r][c] = str(num_nei)
 
         # print(bomb_nei(0,0))
         # print(bomb_nei(2, 1))
@@ -56,5 +58,5 @@ class Solution:
         # print(bomb_nei(1, 3))
         # print(bomb_nei(1, 4))
         
-        return ret
+        return board
         
