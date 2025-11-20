e76fc2cf5d84c1f4bc71f3c51d4709b9f5ef4053
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-11-19 23:33:30 -0800

    Time: 8 ms (46.38%) | Memory: 18.5 MB (75.32%) - LeetSync

---
+++ 860-design-circular-queue/design-circular-queue.py
@@ -0,0 +1,63 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.arr = [0 for _ in range(k)]
+        self.front = 0
+        self.end = 0
+        self.size = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.size == len(self.arr):
+            return False
+
+        if self.size > 0:
+            self.end = (1 + self.end) % len(self.arr)
+
+        # print("before arr", self.arr)
+        # print("before size", self.size)
+        self.arr[self.end] = value
+        self.size += 1
+        # print("after arr", self.arr)
+        # print("after size", self.size)
+        return True        
+
+    def deQueue(self) -> bool:
+        if self.size == 0:
+            return False
+        self.front = (1 + self.front) % len(self.arr)
+        self.size -= 1
+
+        if self.size == 0:
+            self.front = 0
+            self.end = 0
+        return True
+
+    def Front(self) -> int:
+        if self.size == 0:
+            return -1
+        return self.arr[self.front]
+        
+
+    def Rear(self) -> int:
+        if self.size == 0:
+            return -1
+        return self.arr[self.end]
+        
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == len(self.arr)
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
+
+39
