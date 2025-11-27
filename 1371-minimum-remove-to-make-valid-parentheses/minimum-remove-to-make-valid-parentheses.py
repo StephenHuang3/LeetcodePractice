2b24458e7b0a9d7755c20024ddf1efc4a5f563fa
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-11-27 15:01:57 -0800

    Time: 38 ms (72.64%) | Memory: 19.6 MB (33.69%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -3,27 +3,21 @@ class Solution:
         brac = set(['(', ')'])
         ret = []
         stk = []
+        rmv_close = set()
         for i, c in enumerate(s):
             if c in brac:
                 if c == '(':
-                    stk.append(('(', i))
-                    ret.append(('(', i))
+                    stk.append(i)
                 else:
                     if stk:
                         stk.pop()
-                        ret.append(')')
-            else:
+                    else:
+                        rmv_close.add(i)
+
+        rmv_close.update(stk)
+
+        for i, c in enumerate(s):
+            if i not in rmv_close:
                 ret.append(c)
 
-        pos = set([i[1] for i in stk])
-
-        ret2 = []
-        for i in range(len(ret)):
-            if isinstance(ret[i], str):
-                ret2.append(ret[i])
-            elif ret[i][1] in pos:
-                continue
-            else:
-                ret2.append(ret[i][0])
-
-        return "".join(ret2)
+        return "".join(ret)
