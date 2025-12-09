f2842c78030337f767e18409a9530a4908920930
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-12-08 22:16:10 -0800

    Time: 162 ms (31.79%) | Memory: 58 MB (19.93%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,5 +1,6 @@
 import random
 
+
 class RandomizedSet:
 
     def __init__(self):
@@ -9,26 +10,28 @@ class RandomizedSet:
     def insert(self, val: int) -> bool:
         if val in self.hp:
             return False
-        self.hp[val] = len(self.lis)
+
         self.lis.append(val)
+        self.hp[val] = len(self.lis) - 1
         return True
 
     def remove(self, val: int) -> bool:
         if val not in self.hp:
             return False
+
         val_pos = self.hp[val]
         if val_pos != len(self.lis) - 1:
-            exchange_element = self.lis[-1]
+            exch = self.lis[-1]
             self.lis[val_pos], self.lis[-1] = self.lis[-1], self.lis[val_pos]
-            self.hp[exchange_element] = val_pos
+            self.hp[exch] = val_pos
 
         self.lis.pop()
         del self.hp[val]
         return True
 
     def getRandom(self) -> int:
-        return random.choice(self.lis)
-        
+        rand_pos = random.randint(0, len(self.lis) - 1)
+        return self.lis[rand_pos]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
