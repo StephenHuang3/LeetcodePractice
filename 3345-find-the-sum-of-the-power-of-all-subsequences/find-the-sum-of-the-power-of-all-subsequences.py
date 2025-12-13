12fc6be2f622c927279c36ad52a3c6f07ec97d98
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2025-12-13 12:06:49 -0800

    Time: 11 ms (98.15%) | Memory: 18 MB (70.37%) - LeetSync

---
+++ 3345-find-the-sum-of-the-power-of-all-subsequences/find-the-sum-of-the-power-of-all-subsequences.py
@@ -0,0 +1,15 @@
+MOD = 10**9 + 7
+
+
+class Solution:
+    def sumOfPower(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        mod = 10 ** 9 + 7
+        dp = [1] + [0] * k
+        for a in nums:
+            for v in range(k, -1, -1):
+                dp[v] = (dp[v] * 2 + (dp[v - a] if v >= a else 0)) % mod
+        return dp[k]
+
+        print(dp)
+        return dp[k][-1]
