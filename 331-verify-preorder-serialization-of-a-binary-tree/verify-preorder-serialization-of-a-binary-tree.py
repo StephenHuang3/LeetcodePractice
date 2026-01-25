c12fc3b4a073424c37ad7e58173944a10ca7a98d
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2026-01-25 17:12:54 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (33.78%) - LeetSync

--- 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.py
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.py
@@ -1,42 +1,13 @@
 class Solution:
     def isValidSerialization(self, preorder: str) -> bool:
+        spots = 1
         nodes = preorder.split(",")
-        parents = []
-        at_left = True
-        stk = []
 
-        if (len(nodes) > 1 and nodes[0] == "#") or (len(nodes) <= 2 and nodes[0] != "#"):
-            return False
-
-        if nodes[0] == "#" and len(nodes) == 1:
-            return True
-
-        parents.append(nodes[0])
-
-        for node in nodes[1:]:
-            if not parents:
+        for node in nodes:
+            spots -= 1
+            if spots < 0:
                 return False
-            if node == "#":
-                if at_left:
-                    at_left = False
-                else:
-                    parents.pop()
-                    while stk:
-                        popped = stk.pop()
-                        if popped:
-                            break
-                        else:
-                            parents.pop()
-                    at_left = False
-            else:
-                parents.append(node)
-                stk.append(at_left)
-                at_left = True
-            
-        if not parents:
-            return True
-        return False
-
-        
-        # stk: 
-        # at_left: f
+            if node != "#":
+                spots += 2
+        # print(spots)
+        return spots == 0
