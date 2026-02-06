61acead4cfc8ccbec0b1b70042911957380c20d0
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2026-02-05 19:37:32 -0500

    Time: 161 ms (5.84%) | Memory: 38.7 MB (48.17%) - LeetSync

---
+++ 2483-task-scheduler-ii/task-scheduler-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def taskSchedulerII(self, tasks: List[int], space: int) -> int:
+        c = 0
+        last_complete = {}
+        time = 1
+        while c < len(tasks):
+            next_task = tasks[c]
+            prev_complete = -space - 1
+            if next_task in last_complete:
+                prev_complete = last_complete[next_task]
+            if time <= prev_complete + space:
+                time = prev_complete + space + 1
+                continue
+            else:
+                # print(f"day {time} complted {c}")
+                last_complete[next_task] = time
+                time += 1
+                c += 1
+
+        return time - 1
