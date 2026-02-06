012487842293cb9827ca75620704bbb877b178b1
Stephen QiLong Huang <91025265+StephenHuang3@users.noreply.github.com>
2026-02-05 19:38:21 -0500

    Time: 139 ms (13.69%) | Memory: 38.9 MB (11.86%) - LeetSync

--- 2483-task-scheduler-ii/task-scheduler-ii.py
+++ 2483-task-scheduler-ii/task-scheduler-ii.py
@@ -10,11 +10,8 @@ class Solution:
                 prev_complete = last_complete[next_task]
             if time <= prev_complete + space:
                 time = prev_complete + space + 1
-                continue
-            else:
-                # print(f"day {time} complted {c}")
-                last_complete[next_task] = time
-                time += 1
-                c += 1
+            last_complete[next_task] = time
+            time += 1
+            c += 1
 
         return time - 1
